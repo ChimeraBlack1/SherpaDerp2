7ac9c847bf9ee7b0592fdd36008b75f1d9c20151
chimerablack1 <christopherparke@zoho.com>
2019-08-24 23:56:10 -0400

    added error messaging and registration forms and login forms

--- fuckaround/accounts/views.py
+++ fuckaround/accounts/views.py
@@ -1,10 +1,40 @@
 from django.shortcuts import render, redirect
-
-def login(request):
-    return render(request, 'accounts/login.html')
+from django.contrib import messages
+from django.contrib.auth.models import User
 
 def register(request):
-    return render(request, 'accounts/register.html')
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        #check if passwords match
+        if password == password2:
+            #check username
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'That username is taken')
+                return redirect('register')
+            else:
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, 'That emails is being used')
+                    return redirect('register')
+                else:
+                    #looks good
+                    return
+        else:
+            messages.error(request, 'Passwords do not match')
+            return redirect('register')
+    else:
+        return render(request, 'accounts/register.html')
+
+def login(request):
+    if request.method == 'POST':
+        return redirect('dashboard')
+    else:
+        return render(request, 'accounts/login.html')
 
 def logout(request):
     return redirect('index')
---
+++ fuckaround/fuckaround/static/main.js
@@ -0,0 +1,4 @@
+setTimeout(function() {
+    jQuery('#message').fadeOut('slow');
+
+}, 3000);
